4a8fd9c24ee69b0f472280616e9a90b438cb1447
rthibaut <rthibaut@lbl.gov>
2024-03-21 12:25:04 -0700

    Update

--- bnn/robin_lstm_2.py
+++ bnn/robin_lstm_2.py
@@ -3,6 +3,8 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -43,18 +45,33 @@ X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], n_features))
 X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], n_features))
 
 # Define the LSTM model.
+num_components = 3
+output_dim = y_train.shape[1]
+params_size = tfp.layers.MixtureNormal.params_size(num_components, output_dim)  # size of the parameters
+
 # Keep dropout on after model is trained
 model = Sequential([
-    LSTM(50, activation='relu', input_shape=(time_steps, n_features), return_sequences=False),
-    Dropout(0.5),
-    Dense(y_train.shape[1])  # Adjust the number of neurons to match the dimensions of y
+    LSTM(100, activation='tanh', input_shape=(time_steps, n_features), return_sequences=False),
+    Dense(params_size),  # Adjust the number of neurons to match the dimensions of y
+    tfp.layers.MixtureNormal(num_components, output_dim)
 ])
 
+
+def nll(y_true, y_pred):
+    """
+    Negative log likelihood loss function
+    :param y_true: true values
+    :param y_pred: predicted values
+    :return: negative log likelihood
+    """
+    return -y_pred.log_prob(y_true)
+
+
 # Compile the model
-model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.001), loss='mean_squared_error')
+model.compile(optimizer=tf.optimizers.Adam(learning_rate=0.001), loss=nll)
 
 # Fit the model
-early_stopping = EarlyStopping(monitor='val_loss', patience=10, mode='min')
+early_stopping = EarlyStopping(monitor='val_loss', patience=30, mode='min')
 history = model.fit(X_train, y_train, epochs=1000, validation_split=0.1, callbacks=[early_stopping], batch_size=32)
 
 # Plot training history
@@ -77,7 +94,8 @@ x = x.reshape((1, time_steps, n_features))
 # Predict
 n_samples = 1000
 # y_pred = model.predict(x)
-y_pred = np.array([model(x, training=True) for _ in range(n_samples)])
+# y_pred = np.array([model(x, training=True) for _ in range(n_samples)])
+y_pred = np.array(model(x).sample(n_samples)).reshape(n_samples, -1)
 
 # Inverse transform the predictions
 y_pred = scaler_y.inverse_transform(y_pred.reshape(n_samples, -1))
@@ -93,6 +111,6 @@ plt.title("Samples and True Value")
 plt.xlabel("Time Step")
 plt.ylabel("Value")
 plt.legend()
-plt.title(f"Test Sample {test_sample} Predictions with LSTM + Dropout")
+plt.title(f"Test Sample {test_sample} Predictions with LSTM + MDN")
 plt.savefig("lstm_dropout.png", dpi=300, bbox_inches='tight')
 plt.show()
